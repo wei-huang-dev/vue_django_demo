dbab31b1452be4d103f915ce2df952196649bb8c
gusHenriquez <agustin.henriquez@accenturefederal.com>
2022-04-22 16:22:29 -0400

    worked on some of the serializers and they are kind of working when tested with postman

--- DjangoFiles/mainApp/views.py
+++ DjangoFiles/mainApp/views.py
@@ -23,3 +23,29 @@ def book_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def book_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try: 
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = bookSerializer(book)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = bookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return HttpResponse(status=204)
