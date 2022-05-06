87cf073debab655b64b274ba3070eeae04c0e256
gusHenriquez <agustin.henriquez@accenturefederal.com>
2022-05-06 15:02:54 -0400

    changes to gantt chart. Added the details bars

--- vuePracticeApp/pages/testGantt.vue
+++ vuePracticeApp/pages/testGantt.vue
@@ -48,29 +48,29 @@ export default {
           enabled: true,
           buttons: [
             {
-              type: 'day', 
-              count: 7, 
-              text: '7d', 
-              title: 'View 7 Day'
+              type: "day",
+              count: 7,
+              text: "7d",
+              title: "View 7 Day",
             },
             {
-              type: 'month', 
-              count: 1, 
-              text: 'm', 
-              title: 'View 1 Month'
+              type: "month",
+              count: 1,
+              text: "m",
+              title: "View 1 Month",
             },
             {
-              type: 'year', 
-              count: 1, 
-              text: 'y', 
-              title: 'View 1 Year'
+              type: "year",
+              count: 1,
+              text: "y",
+              title: "View 1 Year",
             },
             {
-              type: 'all',  
-              text: 'all', 
-              title: 'View all'
-            }
-          ]
+              type: "all",
+              text: "all",
+              title: "View all",
+            },
+          ],
         },
 
         scrollbar: {
@@ -88,59 +88,105 @@ export default {
                 start: Date.UTC(2017, 11, 1),
                 end: Date.UTC(2017, 11, 1),
                 name: "Class 1",
-                id: "class1"
+                id: "class1",
               },
-              // PLATFORM 1 --------------------------------------------
+              // CLASS 1 PLATFORM 1 --------------------------------------------
               {
                 start: Date.UTC(2017, 11, 1),
                 end: Date.UTC(2018, 1, 2),
                 name: "Platform 1",
                 id: "platform1_1",
-                parent: "class1"
+                parent: "class1",
+                inport: true,
+              },
+              {
+                start: Date.UTC(2017, 11, 1),
+                end: Date.UTC(2018, 1, 2),
+                name: "Platform 1 Details",
+                id: "platform1_1_details",
+                parent: "class1",
+                inport: true,
               },
               {
                 start: Date.UTC(2018, 1, 2),
                 end: Date.UTC(2018, 3, 7),
                 name: "Platform 1",
                 id: "platform1_1",
-                parent: "class1"
+                parent: "class1",
+                inport: false,
               },
-              // PLATFORM 2 --------------------------------------------
+              {
+                start: Date.UTC(2017, 11, 1),
+                end: Date.UTC(2018, 1, 2),
+                name: "Platform 1 Details",
+                id: "platform1_1_details",
+                parent: "class1",
+                inport: true,
+              },
+              // CLASS 1 PLATFORM 2 --------------------------------------------
               {
                 start: Date.UTC(2017, 12, 12),
                 end: Date.UTC(2018, 1, 14),
                 name: "Platform 2",
                 id: "platform2_1",
-                parent: "class1"
+                parent: "class1",
+                inport: false,
+              },
+              {
+                start: Date.UTC(2017, 11, 1),
+                end: Date.UTC(2018, 1, 2),
+                name: "Platform 2 Details",
+                id: "platform1_2_details",
+                parent: "class1",
+                inport: true,
               },
               {
                 start: Date.UTC(2018, 1, 14),
                 end: Date.UTC(2018, 2, 25),
                 name: "Platform 2",
                 id: "platform2_1",
-                parent: "class1"
+                parent: "class1",
+                inport: true,
+              },
+              {
+                start: Date.UTC(2017, 11, 1),
+                end: Date.UTC(2018, 1, 2),
+                name: "Platform 2 Details",
+                id: "platform1_2_details",
+                parent: "class1",
+                inport: true,
               },
               // CLASS 2 --------------------------------------------
               {
                 start: Date.UTC(2017, 11, 1),
                 end: Date.UTC(2017, 11, 1),
                 name: "Class 2",
-                id: "class2"
+                id: "class2",
               },
-              // PLATFORM 1 --------------------------------------------
+              // CLASS 2 PLATFORM 1 --------------------------------------------
               {
                 start: Date.UTC(2017, 11, 10),
                 end: Date.UTC(2017, 12, 5),
                 name: "Platform 1",
                 id: "platform1_2",
-                parent: "class2"
+                parent: "class2",
+                inport: true,
+              },
+              {
+                start: Date.UTC(2017, 11, 1),
+                end: Date.UTC(2018, 1, 2),
+                name: "Platform 1 Details",
+                id: "platform1_1_details",
+                parent: "class2",
+                inport: true,
               },
               {
                 start: Date.UTC(2017, 12, 6),
                 end: Date.UTC(2017, 12, 29),
                 name: "Platform 1",
                 id: "platform1_2",
-                parent: "class2"
+                parent: "class2",
+                inport: false,
               },
             ],
           },
